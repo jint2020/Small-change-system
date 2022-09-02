0ee070d38c97fca576f62b64a43f1aac0b3a6d6f
jint2020 <73622772+jint2020@users.noreply.github.com>
2022-09-02 23:52:24 +0800

    first commit

--- src/com/smallchangesys/Main.java
+++ src/com/smallchangesys/Main.java
@@ -2,6 +2,7 @@ package com.smallchangesys;
 
 public class Main {
     public static void main(String[] args) {
+        // no comments here, dont complain.
         App anApp = new App();
         anApp.startSys();
     }
